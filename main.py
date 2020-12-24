86c79c6424e9b75e1eee3890dc294a821b7238c2
Mortisanti <moltigamer@gmail.com>
2020-12-24 00:27:19 -0500

    main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #TODO Difficulty selection
 #TODO English dictionary, randomly selects word
 #TODO Convert this entire project to Wheel of Fortune: Metalhead Edition
+#TODO Check if input was already used, return appropriate response
 
 secret_word = list("TESTWORD")
-secret_word_progress_before = ["_"] * len(secret_word)
-secret_word_progress_after = ["_"] * len(secret_word)
+secret_word_progress = ["_"] * len(secret_word)
 alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 max_guesses = 6
 game_active = True
 
 print("Welcome to my shitty Hangman game!\n")
-print("The " + str(len(secret_word)) + "-letter word has been chosen: " + " ".join(secret_word_progress_before) + "\n")
+print("The " + str(len(secret_word)) + "-letter word has been chosen: " + " ".join(secret_word_progress) + "\n")
 
 while game_active:
     print("Letters left: " + " ".join(alphabet))
@@ -26,28 +26,26 @@ while game_active:
     for i in range(len(secret_word)):
         if secret_word[i] == guess:
             instances += 1
-            secret_word_progress_after[i] = secret_word[i]
+            secret_word_progress[i] = secret_word[i]
             
     for i in range(len(alphabet)):
 	    if alphabet[i] == guess:
 	        alphabet[i] = "_"
 
-    if secret_word_progress_before == secret_word_progress_after:
+    if instances < 1:
         max_guesses -= 1
         print(f"\nSorry, the letter {guess} is not in the secret word.\n")
+    elif instances == 1:
+        print(f"\nThere is one {guess}.")
     else:
-        secret_word_progress_before = secret_word_progress_after
-        if instances == 1:
-        	print(f"\nThere is one {guess}.")
-        else:
-        	print(f"\nThere are {instances} {guess}'s!")
+       	print(f"\nThere are {instances} {guess}'s!")
         	
     if max_guesses == 0:
-        print("You've run out of guesses... R.I.P.")
+        print("You've run out of guesses... R.I.P.\n")
         break
     
-    if secret_word_progress_after == secret_word:
+    if secret_word_progress == secret_word:
     	print(f"Congratulations! The secret word is: " + "".join(secret_word))
     	break
     else:
-    	print("Current progress: " + " ".join(secret_word_progress_after))
+    	print("Current progress: " + " ".join(secret_word_progress))
